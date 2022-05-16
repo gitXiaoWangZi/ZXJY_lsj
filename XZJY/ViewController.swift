913812dc31f76fdf19b7944437ade58744c6fa75
gitXiaoWangZi <15538509549@163.com>
2022-05-16 20:49:28 +0800

    初始化

---
+++ XZJY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XZJY
+//
+//  Created by 刘圣洁 on 2022/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
